84be45bd692acc3c602b2bdadc7636af906fe174
Stu Bolderson <stu.bolderson@aol.com>
2020-12-12 17:36:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import style
+import './styles/app.scss';
+//import pages
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import images
+import header from '../img/header/header.jpg';
+
+const Home = () => {
+  return (
+    <div className='div'>
+      <h1>Hello</h1>
+      <img src={header} alt='' />
+    </div>
+  );
+};
+
+export default Home;
