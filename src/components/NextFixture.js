a42cffdd85ca9337678be95e09659a1d70cb1070
Stu Bolderson <stu.bolderson@aol.com>
2020-12-15 14:25:31 +0100

    ready to add react carousel to project

--- src/components/NextFixture.js
+++ src/components/NextFixture.js
@@ -1,20 +1,24 @@
 import React from 'react';
+//import images
+import logo from '../img/logos/logo.png';
 
 const NextFixture = () => {
   return (
     <div className='fixture'>
       <div className='fixture__teams'>
-        <div className='team t4q'>
-          <h2>Team4Q</h2>
+        <div className='team home'>
+          <h2>team4q</h2>
         </div>
         <div className='vs'>VS</div>
-        <div className='team'>
-          <h2>Helsingborg</h2>
+        <div className='team away'>
+          <h2>Kalmar</h2>
         </div>
       </div>
-      <div className='fixture__schedule'></div>
+      <div className='fixture__schedule'>
+        <img src={logo} alt='' />
+        <p>18/12/20 @ 16:00 - GA-Hallens, Hbg</p>
+      </div>
     </div>
   );
 };
-
 export default NextFixture;
